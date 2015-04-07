3ef631cffef7147d5dd0867333fd48a6a6a9c969
aifs24 <mathbrain24@hotmail.com>
2015-04-07 15:24:15 -0700

    Base de datos Nuevo Proyecto
    Nuevo proyecto de Visual Studio con la base de datos.

---
+++ Laboratorios CINAP Final/LaboratorioCINAPFinal/LaboratorioCINAPFinal/Controllers/AlumnosController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LaboratorioCINAPFinal.Models;
+using System.Data.Entity.Core.Objects;
+
+namespace LaboratorioCINAPFinal.Controllers
+{
+    public class AlumnosController : Controller
+    {
+        private LaboratoriosCINAPEntities db = new LaboratoriosCINAPEntities();
+
+        // GET: Alumnos
+        public ActionResult Index()
+        {
+            Prueba();
+            var alumnoes = db.Alumnoes.Include(a => a.Persona);
+            return View(alumnoes.ToList());
+        }
+
+        public void Prueba()
+        {
+            ObjectResult < ReporteCarreraSemestre_Result > lol = db.ReporteCarreraSemestre(1, 1);
+            
+            var g = lol.Select(x=> x.ID_Carrera);
+            var lel = 9001;
+        }
+        
+        // GET: Alumnos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Alumno alumno = db.Alumnoes.Find(id);
+            if (alumno == null)
+            {
+                return HttpNotFound();
+            }
+            return View(alumno);
+        }
+
+        // GET: Alumnos/Create
+        public ActionResult Create()
+        {
+            ViewBag.ID_Persona = new SelectList(db.Personas, "ID_Persona", "ID_Persona");
+            return View();
+        }
+
+        // POST: Alumnos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID_Alumno,ID_Persona,Nombre,Matricula")] Alumno alumno)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Alumnoes.Add(alumno);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ID_Persona = new SelectList(db.Personas, "ID_Persona", "ID_Persona", alumno.ID_Persona);
+            return View(alumno);
+        }
+
+        // GET: Alumnos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Alumno alumno = db.Alumnoes.Find(id);
+            if (alumno == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ID_Persona = new SelectList(db.Personas, "ID_Persona", "ID_Persona", alumno.ID_Persona);
+            return View(alumno);
+        }
+
+        // POST: Alumnos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID_Alumno,ID_Persona,Nombre,Matricula")] Alumno alumno)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(alumno).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ID_Persona = new SelectList(db.Personas, "ID_Persona", "ID_Persona", alumno.ID_Persona);
+            return View(alumno);
+        }
+
+        // GET: Alumnos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Alumno alumno = db.Alumnoes.Find(id);
+            if (alumno == null)
+            {
+                return HttpNotFound();
+            }
+            return View(alumno);
+        }
+
+        // POST: Alumnos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Alumno alumno = db.Alumnoes.Find(id);
+            db.Alumnoes.Remove(alumno);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
